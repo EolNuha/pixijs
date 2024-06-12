a7e485d5e0f97b38301d3e37b580ac8f7f83fb7d
eol nuha <olinuha08@hotmail.com>
2024-06-12 18:11:03 +0200

    Import slider component into home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,23 @@
 <script setup>
 import Header from '../components/HeaderComp.vue'
+import ImageSlider from '../components/ImageSlider.vue'
 </script>
 
 <template>
-    <Header />
+  <Header />
+  <div class="main">
+    <ImageSlider />
+  </div>
 </template>
+
+<style lang="scss">
+.main {
+  max-width: 1280px;
+  padding: 0 20px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 100px;
+  padding: 40px 20px;
+}
+</style>
